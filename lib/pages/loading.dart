0146f2fbd220c94419c38e5e2b198ccb4a88f7f2
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-28 23:56:02 +0800

    Learn World Time API

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +11,33 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+
+  void getTime() async {
+    // make the request
+    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
+    Map data = jsonDecode(response.body);
+    // print(data);
+
+    // get properties from data
+    String datetime = data['datetime'];
+    String offset = data['utc_offset'].substring(1,3);
+    // print(datetime);
+    // print(offset);
+
+    // create DateTime object
+    DateTime now = DateTime.parse(datetime);
+    now = now.add(Duration(hours: int.parse(offset)));
+    print(now);
+
+  } 
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getTime();
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
