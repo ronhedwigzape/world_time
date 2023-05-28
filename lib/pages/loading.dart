f6f9370bbc099d25e8dbae92c5f8ecdd369392fb
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-29 00:29:18 +0800

    Learn World Time Custom Class

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-
+import 'package:world_time/services/world_time.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 
@@ -12,36 +12,33 @@ class Loading extends StatefulWidget {
 
 class _LoadingState extends State<Loading> {
 
-  void getTime() async {
-    // make the request
-    Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/Europe/London'));
-    Map data = jsonDecode(response.body);
-    // print(data);
+  String time = 'Loading...';
 
-    // get properties from data
-    String datetime = data['datetime'];
-    String offset = data['utc_offset'].substring(1,3);
-    // print(datetime);
-    // print(offset);
-
-    // create DateTime object
-    DateTime now = DateTime.parse(datetime);
-    now = now.add(Duration(hours: int.parse(offset)));
-    print(now);
-
-  } 
+  void setupWorldTime() async {
+    WorldTime instance = WorldTime(
+        location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
+    await instance.getTime();
+    // Navigator.pushReplacementNamed(context, '/home');
+    print(instance.time); 
+    setState(() {
+      time = instance.time;
+    });
+  }
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    getTime();
+    setupWorldTime();
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Loading Screen'),
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Text(time),
+      ),
     );
   }
 }
---
+++ lib/services/world_time.dart
@@ -0,0 +1,45 @@
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class WorldTime {
+  
+    String location; // location name for the UI
+    late String time; // the time in that location
+    String flag; // url to an asset flag icon
+    String url; // location url for api endpoint
+    late bool isDaytime; // true or false if daytime or not
+  
+    WorldTime({required this.location, required this.flag, required this.url});
+  
+    Future<void> getTime() async {
+  
+      try {
+        // make the request
+        Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
+        Map data = jsonDecode(response.body);
+        // print(data);
+  
+        // get properties from data
+        String datetime = data['datetime'];
+        String offset = data['utc_offset'].substring(1,3);
+        // print(datetime);
+        // print(offset);
+  
+        // create DateTime object
+        DateTime now = DateTime.parse(datetime);
+        now = now.add(Duration(hours: int.parse(offset)));
+  
+        // set the time property
+        isDaytime = now.hour > 6 && now.hour < 20 ? true : false;
+        time = now.toString();
+      }
+      catch (e) {
+        print('caught error: $e');
+        time = 'could not get time data';
+      }
+  
+    }
+}
+
+//
+WorldTime instance = WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin');
