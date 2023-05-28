eb38fba41088b091db2f6cf8aa2a7163b0c7dccf
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-28 22:55:35 +0800

    Learn Async in Flutter

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -8,32 +8,37 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  void getData() async {
+    // simulate network request for a username
+    String username = await Future.delayed(const Duration(seconds: 3), () {
+      return 'Yoshi';
+    });
+
+    String bio = await Future.delayed(const Duration(seconds: 2), () {
+      return 'vegan, musician & egg collector';
+    });
+
+    print('$username - $bio');
+  }
 
   @override
   void initState() {
     super.initState();
-    print('initState function ran');
+    getData();
+    print('hey there!');  
   }
 
   @override
   Widget build(BuildContext context) {
-    print('build function ran');
     return Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          title: const Text('Choose a Location'),
-          centerTitle: true,
-          elevation: 0,
-        ),
-        body: ElevatedButton(
-          onPressed: () {
-            setState(() {
-              counter += 1;
-            });
-          },
-          child: Text('Counter is $counter'),
-        ));
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: const Text('Choose a Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      body: const Text(''),
+    );
   }
 }
