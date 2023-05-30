d0e2c52163053812eb541a08c3281dc8f953d2a5
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-05-30 18:10:05 +0800

    Learn Updating Time
    - Finished the Flutter Tutorials with TheNetNinja

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -9,7 +9,6 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-
   List<WorldTime> locations = [
     WorldTime(location: 'Berlin', flag: 'germany.png', url: 'Europe/Berlin'),
     WorldTime(location: 'Athens', flag: 'greece.png', url: 'Europe/Athens'),
@@ -21,42 +20,47 @@ class _ChooseLocationState extends State<ChooseLocation> {
     WorldTime(location: 'Jakarta', flag: 'indonesia.png', url: 'Asia/Jakarta'),
   ];
 
+  void updateTime(index) async {
+    WorldTime instance = locations[index];
+    await instance.getTime();
+    // navigate to home screen
+    Navigator.pop(context, {
+      'location': instance.location,
+      'flag': instance.flag,
+      'time': instance.time,
+      'isDaytime': instance.isDaytime
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: const Text('Choose a Location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      body: ListView.builder(
-        itemCount: locations.length,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
-            child: Card(
-              child: ListTile(
-                onTap: () async {
-                  WorldTime instance = locations[index];
-                  await instance.getTime();
-                  Navigator.pop(context, {
-                    'location': instance.location,
-                    'flag': instance.flag,
-                    'time': instance.time,
-                    'isDaytime': instance.isDaytime
-                  });
-                },
-                title: Text(locations[index].location),
-                leading: CircleAvatar(
-                  backgroundImage: AssetImage('assets/${locations[index].flag}'),
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: const Text('Choose a Location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        body: ListView.builder(
+            itemCount: locations.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+                child: Card(
+                  child: ListTile(
+                    onTap: () async {
+                      updateTime(index);
+                    },
+                    title: Text(locations[index].location),
+                    leading: CircleAvatar(
+                      backgroundImage:
+                          AssetImage('assets/${locations[index].flag}'),
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          );
-        }
-      )
-    );
+              );
+            }));
   }
 }
